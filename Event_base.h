df122f9b6d0c9e49ffaf7ccbce8b34d8ea3eeb61
jooney <vikingpanzer@sina.com>
2017-12-15 17:33:06 +0800

    CMaketest

---
+++ Event_base.h
@@ -0,0 +1,13 @@
+#ifndef __EVENT_BASE_H__
+#define __EVENT_BASE_H__
+#include "Util.h"
+
+class EventBases : public noncopyable
+{
+//	virtual EventBase* allocBase() = 0;	
+};
+
+
+
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include "Util.h"
+#include "Event_base.h"
+int main()
+{
+	return 0;
+}
